6eca5098ff5b15a5f696f302337eeee8dd6f4122
brixxt27 <brixxt27@gmail.com>
2022-12-10 16:39:36 +0900

    feat: 문자열을 변수에 담아 동적으로 페이지 요소 변경

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
-var http = require('http');
-var fs = require('fs');
-// var url = require('url');
+/**
+ * require 내에 있는 http, fs, url 들은 모듈이라고 하며, node.js가 가지고 있는 수많은 기능 중
+ * 비슷한 것끼리 묶어 둔 것이다.
+ */
+let http = require('http');
+let fs = require('fs');
+let url = require('url');
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    console.log(_url);
-    if(_url == '/'){
-      _url = '/index.html';
-    }
-    if(_url == '/favicon.ico'){
-      response.writeHead(404);
-      response.end();
-      return ;
-    }
-    response.writeHead(200);
-    // 프로그램적으로 사용자에게 전송할 데이터를 생성한다.
-    response.end(fs.readFileSync(__dirname + _url));
+let app = http.createServer(function(request, response){
+  let _url = request.url;
+  let queryData = url.parse(_url, true).query;
+  let title = queryData.id;
+  let template = `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="index.html">WEB</a></h1>
+    <ol>
+      <li><a href="/?id=HTML">HTML</a></li>
+      <li><a href="/?id=CSS">CSS</a></li>
+      <li><a href="/?id=JavaScript">JavaScript</a></li>
+    </ol>
+    <h2>${title}</h2>
+    <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+    <img src="coding.jpg" width="100%">
+    </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
+    </p>
+  </body>
+  </html>
+`;
+  
+  if (_url == '/'){
+    _url = '/index.html';
+  }
+  if (_url == '/favicon.ico'){
+    return (response.writeHead(404));
+  }
+  response.writeHead(200);
+  response.end(template);
 });
 app.listen(3000);
