8806881cbd086c60ce744b645c6d65705620b6cf
brixxt27 <brixxt27@gmail.com>
2023-07-01 01:03:08 +0900

    feat: template literal 연습

---
+++ syntax/template_literal.js
@@ -0,0 +1,11 @@
+const word = 'jayoon';
+
+/** 개행을 사용하기 위해 n이전에 역슬래시를 넣는데 이때 역슬래시 \ 를 두 개 넣으면 개행으로 인식하지 못한다. */
+let str = 'Dear ' + word + ' Lorem ipsum dolor sit amet,\n\
+\n\
+ consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' + word + ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ' + word + ' Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur ' + word + ' sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+
+ /** 변수를 나타내기 위해서는 중괄호 {} 밖에 사용하지 못한다. 소괄호나 대괄호는 단순히 문자로 인식함. */
+let letter = `Dear ${word} Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ${word} Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ${word} Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur ${word} sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`
+console.log(str + '\n\n\n');
+console.log(letter);
