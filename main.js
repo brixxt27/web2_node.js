790fe56d4f30ecdd2a06e495452ca273ca3f4f67
brixxt27 <brixxt27@gmail.com>
2023-07-01 03:57:48 +0900

    feat: query string의 값으로 title 변경
    query string에는 이름과 값이 있는데 여기서 값만을 사용하여 특정 하이퍼
    텍스트를 클릭했을 때 그 하이퍼 텍스트의 데이터로 title 을 변경하기로
    했다. 그렇게 하기 위해 하이퍼 텍스트를 클릭하면 /?id='하이퍼 텍스트의
    데이터' 형식으로 이동하도록 했고, 이를 파싱하여 이용한다.

--- main.js
+++ main.js
@@ -5,17 +5,40 @@ var url = require("url");
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   const parseData = url.parse(_url, true).query;
-  console.log(parseData.id);
+  var title = parseData.id;
+
+  console.log(title);
 
   if (_url == "/") {
-    _url = "/index.html";
+    title = "Welcome";
   }
   if (_url == "favicon.ico") {
     response.writeHead(404);
     response.end();
     return;
   }
+
+  var template = `<!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    <ol>
+      <li><a href="/?id=HTML">HTML</a></li>
+      <li><a href="/?id=CSS">CSS</a></li>
+      <li><a href="/?id=JavaScript">JavaScript</a></li>
+    </ol>
+    <h2>${title}</h2>
+    <p>The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
+    </p>
+  </body>
+  </html>
+  `;
+
   response.writeHead(200);
-  response.end(parseData.id);
+  response.end(template);
 });
 app.listen(3000);
